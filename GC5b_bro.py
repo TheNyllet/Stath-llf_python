af615f8ccb76e4fae872de3d6f6cea2f699932c4
TheNyllet <hugo@mindata.se>
2025-03-05 16:16:31 +0100

    Update GC5b_bro.py

--- GC5b_bro.py
+++ GC5b_bro.py
@@ -7,10 +7,11 @@ import matplotlib.pyplot as plt
 M_max = 518.9e6 # Maximalt böjmoment [Nmm], från GC4c
 I_y = 758986666.7  # Yttröghetsmoment [mm^4], från GC5a
 h_total = 560  # Total höjd av I-tvärsnittet [mm] (520 mm liv + 2 * 20 mm flänsar)
-z = np.linspace(-h_total/2, h_total/2, 100)  # z-koordinater [mm]
+d = np.linspace(-h_total/2, h_total/2, 100)  # Avstånd från mittpunkt [mm]
+z = np.linspace(0, h_total, 100)  # z-koordinat [mm]
 
 # Normalspänning
-sigma_z = (M_max * z) / I_y  # Normalspänning [MPa]
+sigma_z = (M_max * d) / I_y  # Normalspänning [MPa]
 
 # Maximal drag- och tryckspänning
 sigma_max_tension = np.max(sigma_z)  # Maximal dragspänning
@@ -18,7 +19,6 @@ sigma_max_compression = np.min(sigma_z)  # Maximal tryckspänning
 
 print(f"Maximal dragspänning: {sigma_max_tension:.2f} MPa vid z = {z[np.argmax(sigma_z)]:.2f} mm")
 print(f"Maximal tryckspänning: {sigma_max_compression:.2f} MPa vid z = {z[np.argmin(sigma_z)]:.2f} mm")
-print(f'Detta sker vid x = {z[np.argmax(sigma_z)]/h_total:.2f}h och x = {z[np.argmin(sigma_z)]/h_total:.2f}h')
 
 # Plotta normalspänning
 plt.plot(z, sigma_z, label="Normalspänning")
